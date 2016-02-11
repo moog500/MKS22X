e779d130b7e8148ad3341470e9d66d0edef0f683
moog500 <dakim@stuy.edu>
2016-02-10 22:13:42 -0500

    working

--- 02nqueens/QueenBoard.java
+++ 02nqueens/QueenBoard.java
@@ -20,7 +20,13 @@ public class QueenBoard {
     {
 	return false;
     }
-    public void solveW(int row){
+    public void solveH(int col){
+    	for (int i = 0; i < board.length; i++){
+    		if (i == 0 && col == 0){
+    			if (board[0][1] = 0 && )
+    		}
+    	}
+
   /*  	try {
     	for (int i = 0; i < board[row].length; i++){
     		if (board[row][i] == 0 && board[row + 1][i] != 1 && board[row][i + 1] != 1 && board[row + 1][i + 1] != 1 && board[row+ 1][i-1] != 1){
